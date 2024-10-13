96ada479fb2d4e9bf99daec85e7d19a8d077c1cd
arek190 <szablanarkadiusz98@gmail.com>
2024-10-13 09:34:00 +0200

    tes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Sznycel");
+/*
+ * Komentarze
+ */
+
+Console.WriteLine(3+3);
+
+int a, b = 34;
+double c = 34.34;
+short s = 1;
+long longbA = 12312312312312;
+char znak = 'a';
+string name1 = "jan";
+bool type = true;
+
+
+//Console.WriteLine("wartość liczby b = " + b);
+//Console.WriteLine("s = " + s);
+
+
+//Console.WriteLine("Podaj imię: ");
+//string name = Console.ReadLine();
+//Console.WriteLine("Twoje imię: " + name);
+
+//Console.WriteLine("Podaj liczbę: ");
+//int number = Convert.ToInt32( Console.ReadLine());
+//Console.WriteLine("Twoja liczba: " + number);
+
+//int liczba  = 10;
+//String tekst = "jan";
+Console.WriteLine("Podaj liczbę: ");
+string input = Console.ReadLine();
+int liczba = int.Parse(input);
