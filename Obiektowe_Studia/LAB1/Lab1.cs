6ae288a7e7403fc40d1fc4c545cebfcd3b35c2da
Arek190 <szablanarkadiusz98@gmail.com>
2024-11-14 18:19:54 +0100

    Dodanie_LAB2

---
+++ Obiektowe_Studia/LAB1/Lab1.cs
@@ -0,0 +1,25 @@
+﻿namespace LAB1
+{
+    public class Lab1
+    {
+        public static void RunLab()
+        {
+            Console.WriteLine("Wybierz zadanie z LAB1 (1-7): ");
+            int taskChoice = Convert.ToInt32(Console.ReadLine());
+
+            switch (taskChoice)
+            {
+                case 1: Zad_1.RUN(); break;
+                case 2: Zad_2.RUN(); break;
+                case 3: Zad_3.RUN(); break;
+                case 4: Zad_4.RUN(); break;
+                case 5: Zad_5.RUN(); break;
+                case 6: Zad_6.RUN(); break;
+                case 7: Zad_7.RUN(); break;
+                default:
+                    Console.WriteLine("Błędny wybór zadania w LAB1.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Obiektowe_Studia/LAB2/Lab2.cs
@@ -0,0 +1,19 @@
+﻿namespace LAB2
+{
+    public class Lab2
+    {
+        public static void RunLab()
+        {
+            Console.WriteLine("Wybierz zadanie z LAB2 (1-x): ");
+            int taskChoice = Convert.ToInt32(Console.ReadLine());
+
+            switch (taskChoice)
+            {
+                //case 1: Zad_2.RUN(); break;
+                default:
+                    Console.WriteLine("Błędny wybór zadania w LAB2.");
+                    break;
+            }
+        }
+    }
+}
