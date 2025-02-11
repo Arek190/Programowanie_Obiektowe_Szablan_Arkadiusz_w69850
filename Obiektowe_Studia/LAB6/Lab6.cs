2f3fa98785a3c4fd3832dbaed0b6446543a395c6
Arek190 <szablanarkadiusz98@gmail.com>
2025-02-11 19:00:35 +0100

    Lab6

---
+++ Obiektowe_Studia/LAB6/Lab6.cs
@@ -0,0 +1,18 @@
+﻿namespace MojeZadania.LAB6;
+
+public class Lab6
+{
+    public static void RunLab()
+    {
+        Console.WriteLine("Wybierz zadanie z LAB1 (1-7): ");
+        int taskChoice = Convert.ToInt32(Console.ReadLine());
+
+        switch (taskChoice)
+        {
+            case 1:  ; break;
+            default:
+                Console.WriteLine("Błędny wybór zadania w LAB3.");
+                break;
+        }
+    }
+}
